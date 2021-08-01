bc90cc693d123e78aa4d9cb493d733e46f95151e
miomaeshima <miomamomo@gmail.com>
2021-08-01 23:07:39 +0900

    edited Japanese intro text

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -27,28 +27,49 @@ const Main = () => {
         text =
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     } else {
-        text = <p>日本の伝統色には味わい深い名前がいっぱい。こちらのサイトでは、お手持ちの画像の中にどんな伝統色があるのか、画像を選び、クリックするだけで調べられます。ただ青でなく「勿忘草（わずれなぐさ）」、赤ではなく「梅重（うめかさね）」、、、ぜひ繊細な名前を見つけてみてください。<br/>趣のある色の名前を持つのは日本語だけではありません。右上のメニューから、英語、フランス語のパレットにも切り替えられます。<br/>このサイトでは、色について二つの選び方を用意しています。一つはMain Colorというページで、画像で一番使われている色について調べられます。もう一つはAny Colorというページで、こちらでは、画像上のどこでもクリックした場所の色について調べることができます。<br/>どちらも、選んだパレットから、調べたい色か、その色がなければ、それに一番近い色が選びだされて返ってきます。「日本の伝統色」、「English」、「Français」の各パレットにある色の数や種類が違うのでパレットによって「一番近い色」も変わってきます。実際にパレット／言語を切り替えてみると、返ってくる色の名前が違うことに気づかれると思います。</p>;
+        text = (
+            <>
+                <p>
+                    日本の伝統色には味わい深い名前がいっぱい。こちらのサイトでは、お手持ちの画像の中にどんな伝統色があるのか、画像を選び、クリックするだけで調べられます。*
+                </p>
+                <p>
+                    趣のある色の名前を持つのは日本語だけではありません。画面右上の「日本の伝統色」と出ているプルダウンメニューから、英語、フランス語のパレットにも切り替えられます。
+                </p>
+                <p>
+                    このサイトでは、色について二つの選び方を用意しています。一つはMain
+                    Colorというページで、画像で一番使われている色について調べられます。もう一つはAny
+                    Colorというページで、こちらでは、画像上のどこでもクリックした場所の色について調べることができます。どちらのページも、選んだパレットから、調べたい色か、その色がなければ、それに一番近い色が選びだされて返ってきます。
+                </p>
+                <p>
+                    「日本の伝統色」、「English」、「Français」の各パレットにある色の数や種類が違うのでパレットによって「一番近い色」も変わってきます。実際にパレット／言語を切り替えると、返ってくる色と名前が変わるのに気づかれるでしょう。
+                </p>
+                <p>
+                    ＊
+                    画像を画面上に表示し、クリックしても、サーバー側に送られるのは調べたい色のRGB値だけで、画像自体はどこにも送られません。
+                </p>
+              
+            </>
+        );
     }
 
     return (
         <Wrapper>
             <Title>{title}</Title>
-            {text}
-            <ButtonContainer />
+            <Intro>{text} <ButtonContainer /></Intro>
+           
         </Wrapper>
     );
 };
 
 const Wrapper = styled.div`
     position: relative;
-    width: 70%;
-    height: 300px;
+    width: 73%;
+
     margin-top: 100px;
     margin-left: auto;
     margin-right: auto;
-    padding-top: 21px;
-    padding-left: 32px;
-    background: rgba(0, 0, 150, 0.1);
+    padding: 30px 42px 32px 42px;
+    background: rgba(0, 0, 150, 0.125);
 
     display: flex;
     flex-direction: column;
@@ -58,13 +79,19 @@ const Wrapper = styled.div`
 
 const Title = styled.div`
     font-family: '游明朝', 'Yu Mincho', YuMincho, 'Hiragino Mincho Pro', serif;
-    font-size: 48px;
+    font-size: 56px;
 `;
 
-const P = styled.p`
-padding-left: 5px;
-    max-width: 50ch;
+const Intro = styled.div`
+    padding-left: 5px;
+    columns: 2;
+    column-gap: 48px;
+
     color: white;
+    p {
+        margin-bottom: 16px;
+        break-inside: avoid;
+    }
 `;
 
 export default Main;
