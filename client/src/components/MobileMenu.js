838a7f12706acbe3499600c7b11a5c5d388d40bf
miomaeshima <miomamomo@gmail.com>
2021-08-18 23:39:20 +0900

    edited layout of MobileMenu Modal

--- client/src/components/MobileMenu.js
+++ client/src/components/MobileMenu.js
@@ -1,16 +1,89 @@
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
-import "@reach/dialog/styles.css";
+import '@reach/dialog/styles.css';
+import { NavLink } from 'react-router-dom';
+import { NavHashLink } from 'react-router-hash-link';
+import { X } from 'react-feather';
+import { COLORS } from '../constants';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
     return (
         <DialogOverlay isOpen={isOpen} onDismiss={onDismiss}>
-            <DialogContent>
-                <p>Moldal</p>
-                <button onClick={onDismiss}>Close</button>
-            </DialogContent>
+            <StyledDialogContent aria-label="Modal">
+                <StyledX
+                    size={'1.5rem'}
+                    strokeWidth={2}
+                    onClick={onDismiss}
+                    tabIndex={0}
+                />
+                <NavLinkWrapper>
+                    <StyledNavLink
+                        activeStyle={{ borderBottom: '1px solid' }}
+                        exact
+                        to="/"
+                    >
+                        Home
+                    </StyledNavLink>
+                    <StyledNavHashLink
+                        smooth
+                        activeStyle={{ borderBottom: '1px solid' }}
+                        to="/#about"
+                    >
+                        About
+                    </StyledNavHashLink>
+                    <StyledNavLink
+                        activeStyle={{ borderBottom: '1px solid' }}
+                        to="/main_color"
+                    >
+                        MainColor
+                    </StyledNavLink>
+                    <StyledNavLink
+                        activeStyle={{ borderBottom: '1px solid' }}
+                        to="/any_color"
+                    >
+                        AnyColor
+                    </StyledNavLink>
+                </NavLinkWrapper>{' '}
+            </StyledDialogContent>
         </DialogOverlay>
     );
 };
+
+const StyledDialogContent = styled(DialogContent)`
+    width: 60%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    right: 0;
+    margin-top: 0;
+    padding: 48px;
+    padding-bottom: 120px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    background: ${COLORS.Gray};
+`;
+
+const StyledNavLink = styled(NavLink)`
+    width: fit-content;
+    text-decoration: none;
+`;
+const StyledNavHashLink = styled(NavHashLink)`
+    width: fit-content;
+    text-decoration: none;
+`;
+
+const StyledX = styled(X)`
+    position: fixed;
+    top: 8px;
+    right: 8px;
+`;
+
+const NavLinkWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 48px;
+    font-size: 1.2rem;
+`;
 
 export default MobileMenu;
